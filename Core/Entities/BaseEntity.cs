d7757f4c88c9e655e67f218e634b7d18b3d0906c
Felix Battig <fbattig@kreonovo.ca>
2020-08-11 06:09:32 -0400

    Initial Commit

---
+++ Core/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Core.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        // public DateTime CreationDate { get; set; }
+        // public DateTime UpdateTime { get; set; }
+    }
+}
